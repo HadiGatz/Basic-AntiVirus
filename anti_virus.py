0fef35383ad667fcf1f9f106804d6de12571ed79
Hadi Gatz <albertsviridovski@gmail.com>
2024-07-18 00:12:52 +0300

    [ADD] added a function to check if a file is corrupted, added a request from the API, and a basic analysis of the responce

--- anti_virus.py
+++ anti_virus.py
@@ -8,3 +8,19 @@ headers = {
     "x-apikey": API_KEY
 }
 
+file = open(input("Enter your file name: "))
+
+def check_if_corrupted():
+    try:
+        file.read()
+    except:
+        print("The file is corrupt.")
+
+response = requests.get(url, files={'file': file}, headers=headers)
+
+if response.status_code == 200:
+    print("File successfully uploaded and scanned.")
+    scan_results = response.json()
+    print(scan_results)  
+else:
+    print("Error")
