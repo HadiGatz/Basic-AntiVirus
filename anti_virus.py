df11f7b2b7601b720c62a7c1475ff05668c0b792
Hadi Gatz <albertsviridovski@gmail.com>
2024-07-18 13:36:41 +0300

    [ADD] added functions for requests to the API, and a formatted printing function

--- anti_virus.py
+++ anti_virus.py
@@ -1,4 +1,6 @@
 import requests
+import sys
+from termcolor import colored, cprint
 
 url = 'https://www.virustotal.com/api/v3/files'
 
@@ -7,9 +9,7 @@ API_KEY = "d6970bf852d449af239cc1e7026f11ecdf69c5189ff6451c173068ef6e311c9c"
 headers = {
     "x-apikey": API_KEY
 }
-headers3 = {
-    "accept" : "application/json"
-}
+
 
 file = open(input("Enter your file name: "), "rb")
 
@@ -19,13 +19,29 @@ def check_if_corrupted():
     except:
         print("The file is corrupt.")
 
-response = requests.post(url, files={'file': file}, headers=headers)
+def scan_file(file):
+    response = requests.post(url, files={'file': file}, headers=headers)
+    return response
+
+def retrieve_scan_results(url):
+    analysis = requests.get(url=url, headers=headers)
+    return analysis
+
+def print_analysis_results(analysis):
+    print(colored("Your file was checked by 70+ Anti-Virus softwares.", "green"))
+    print("------------------------------------------")
+    for x, y in analysis.items():
+        if y > 0:
+            print(x, colored(y, "red"))
+        elif y == 0:
+            print(x, colored(y, "green"))
+response = scan_file(file)
 
 if response.status_code == 200:
     print("File successfully uploaded and scanned.")
     scan_results = dict(response.json())
     scan_url = scan_results['data']['links']['self']
-    analysis = requests.get(url=scan_url, headers=headers)  
-    print(analysis.json())
+    analysis = retrieve_scan_results(scan_url) 
+    print_analysis_results(analysis.json()['data']['attributes']['stats'])
 else:
     print("Error")
