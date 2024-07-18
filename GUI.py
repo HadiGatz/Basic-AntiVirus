eea9e7090476ece26b18d88cfea387c42b378b80
Hadi Gatz <albertsviridovski@gmail.com>
2024-07-18 16:36:15 +0300

    [ADD] added some text to the GUI

--- GUI.py
+++ GUI.py
@@ -8,8 +8,18 @@ size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("AKAntiVirus")
 
+screen.fill((119, 195, 165))
+pygame.display.flip()
+
+pygame.font.init()
+my_font = pygame.font.SysFont('Adobe', 80)  # Increased font size to 60
+text_surface = my_font.render('AKAntiVirus', False, (255, 255, 255))
+
 run = True
 while run:
+    screen.fill((119, 195, 165)) 
+    screen.blit(text_surface, ((SCREEN_WIDTH / 4), 40))  
+    pygame.display.flip()  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
