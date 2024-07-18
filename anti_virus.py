4edf186a110c04ce217c840e05026a2bcf9b55ba
Hadi Gatz <albertsviridovski@gmail.com>
2024-07-18 12:04:27 +0300

    [ADD] retrieved the scan link and requested the actual analysis using get()

--- anti_virus.py
+++ anti_virus.py
@@ -2,10 +2,13 @@ import requests
 
 url = 'https://www.virustotal.com/api/v3/files'
 
-API_KEY = ""
+API_KEY = "d6970bf852d449af239cc1e7026f11ecdf69c5189ff6451c173068ef6e311c9c"
 
 headers = {
     "x-apikey": API_KEY
+}
+headers3 = {
+    "accept" : "application/json"
 }
 
 file = open(input("Enter your file name: "), "rb")
@@ -20,7 +23,9 @@ response = requests.post(url, files={'file': file}, headers=headers)
 
 if response.status_code == 200:
     print("File successfully uploaded and scanned.")
-    scan_results = response.json()
-    print(scan_results)  
+    scan_results = dict(response.json())
+    scan_url = scan_results['data']['links']['self']
+    analysis = requests.get(url=scan_url, headers=headers)  
+    print(analysis.json())
 else:
     print("Error")
